1bb3263c9deb0e883bb9a669b9b10011eeb8e9f3
gloscherrybomb <gloscherrybomb@gmail.com>
2026-04-22 10:30:30 -0400

    Bump version to 0.4.2 (versionCode 8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.tymewear.karoo"
         minSdk = 26
         targetSdk = 34
-        versionCode = 7
-        versionName = "0.4.1"
+        versionCode = 8
+        versionName = "0.4.2"
     }
 
     buildTypes {
